b98272b8feccc006585c975ca713a4e6db51b468
SAYED SEYAB <141121213+Sayyed-Seyab@users.noreply.github.com>
2025-02-10 10:21:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const allowedOrigins = [
     'http://localhost:5174',
     'http://localhost:5175',
     'http://localhost:5176',
-   'https://dashboard-app-2clxs.ondigitalocean.app/',
+   'https://seahorse-app-e3fxr.ondigitalocean.app',
   ];
   
   app.use(
