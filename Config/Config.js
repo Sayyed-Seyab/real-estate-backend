f29f5129e4d6fe57bf64823a33c5ca57ea6348ff
SAYED SEYAB <141121213+Sayyed-Seyab@users.noreply.github.com>
2025-02-13 13:17:05 +0500

    Update Config.js

--- Config/Config.js
+++ Config/Config.js
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose'
 const Db = ()=>{
-    mongoose.connect("mongodb+srv://admin2:admin2@Cluster0.uaxmezr.mongodb.net/HasooManagement").then(()=>{
-        console.log('Database is connected')
-        // generateSchema('agents');
-    },
-err =>{console.log('There is a problem while connecting Database' + err)});
-console.log('db file')
-// const dbURI = "mongodb+srv://admin2:admin2@Cluster0.uaxmezr.mongodb.net/HasooManagement";
+//     mongoose.connect("mongodb+srv://admin2:admin2@Cluster0.uaxmezr.mongodb.net/HasooManagement").then(()=>{
+//         console.log('Database is connected')
+//         // generateSchema('agents');
+//     },
+// err =>{console.log('There is a problem while connecting Database' + err)});
+// console.log('db file')
+// // const dbURI = "mongodb+srv://admin2:admin2@Cluster0.uaxmezr.mongodb.net/HasooManagement";
 
-// mongoose.connect(dbURI)
-//   .then(() => console.log("Database connected"))
-//   .catch((err) => console.log("There is a problem while connecting database", err));
-}
+// // mongoose.connect(dbURI)
+// //   .then(() => console.log("Database connected"))
+// //   .catch((err) => console.log("There is a problem while connecting database", err));
+// }
 
 
 
