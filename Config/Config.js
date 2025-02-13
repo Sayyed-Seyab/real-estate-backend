1c6070845981981c791ba065172a3f7c7d839bf2
SAYED SEYAB <141121213+Sayyed-Seyab@users.noreply.github.com>
2025-02-13 14:06:46 +0500

    Update Config.js

--- Config/Config.js
+++ Config/Config.js
@@ -1,5 +1,21 @@
-// import mongoose from 'mongoose'
-const Db = ()=>{
+const { MongoClient } = require('mongodb');
+const Db = async  ()=>{
+
+  const uri = process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/HasooManagement";
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        const databases = await client.db().admin().listDatabases();
+        console.log("Connected successfully. Databases:");
+        databases.databases.forEach(db => console.log(` - ${db.name}`));
+    } catch (e) {
+        console.error("Error connecting to MongoDB:", e);
+    } finally {
+        await client.close();
+    }
+
+ 
 //     mongoose.connect("mongodb+srv://admin2:admin2@Cluster0.uaxmezr.mongodb.net/HasooManagement").then(()=>{
 //         console.log('Database is connected')
 //         // generateSchema('agents');
@@ -12,6 +28,11 @@ const Db = ()=>{
 // //   .then(() => console.log("Database connected"))
 // //   .catch((err) => console.log("There is a problem while connecting database", err));
 // }
+
+
+
+
+
 
 
 
