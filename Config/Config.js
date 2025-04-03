496b25baa485ab444d79e778d3d23cb436c302a8
Sayyed-Seyab <syed.seyab767@gmail.com>
2025-04-03 21:56:45 +0500

    api structure add

--- Config/Config.js
+++ Config/Config.js
@@ -1,16 +1,16 @@
 import mongoose from 'mongoose'
 const Db = ()=>{
-    // mongoose.connect("mongodb://127.0.0.1:27017/HasooManagement").then(()=>{
-    //     console.log('Database is connected')
-        // generateSchema('agents');
-    // },
-// err =>{console.log('There is a problem while connecting Database' + err)});
-// console.log('db file')
-const dbURI = "mongodb+srv://admin2:admin2@Cluster0.uaxmezr.mongodb.net/HasooManagement";
+    mongoose.connect("mongodb://127.0.0.1:27017/HasooManagement").then(()=>{
+        console.log('Database is connected')
+        generateSchema('agents');
+    },
+err =>{console.log('There is a problem while connecting Database' + err)});
+console.log('db file')
+// const dbURI = "mongodb+srv://admin2:admin2@Cluster0.uaxmezr.mongodb.net/HasooManagement";
 
-mongoose.connect(dbURI)
-  .then(() => console.log("Database connected"))
-  .catch((err) => console.log("There is a problem while connecting database", err));
+// mongoose.connect(dbURI)
+//   .then(() => console.log("Database connected"))
+//   .catch((err) => console.log("There is a problem while connecting database", err));
 }
 
 
